f46f64a7b70475acee94c949d58a23bc9e48a379
guisystem <guilherme03102000@gmail.com>
2024-11-16 18:07:45 -0300

    Mudanças no SalvarSenhaController
    - Foi adicionada a palavra-chave final na tela, helper e EntityManager.
    - Foi aberta a conexão e feito o commit para atualizar a senhas do usuário.
    - Antes de salvar, foi adicionado um método para criptografa-la.

--- codigos/geradorDeSenha-Interface/src/rocha/guilherme/jose/controller/SalvarSenhaController.java
+++ codigos/geradorDeSenha-Interface/src/rocha/guilherme/jose/controller/SalvarSenhaController.java
@@ -8,14 +8,15 @@ import rocha.guilherme.jose.model.ModelSenhasSalvas;
 import rocha.guilherme.jose.model.ModelUsuario;
 import rocha.guilherme.jose.model.dao.SenhasSalvasDAO;
 import rocha.guilherme.jose.model.dao.UsuarioDAO;
+import rocha.guilherme.jose.servico.Criptografar;
 import rocha.guilherme.jose.servico.JPAConnection;
 import rocha.guilherme.jose.view.SalvarSenhaView;
 
 public class SalvarSenhaController {
 
-	private SalvarSenhaView salvarSenhaView;
-	private SalvarSenhaHelper helper;
-	private EntityManager em;
+	private final SalvarSenhaView salvarSenhaView;
+	private final SalvarSenhaHelper helper;
+	private final EntityManager em;
 	
 	public SalvarSenhaController(SalvarSenhaView salvarSenhaView) {
 		this.salvarSenhaView = salvarSenhaView;
@@ -43,22 +44,30 @@ public class SalvarSenhaController {
 	}
 
 	private void atualizarOuSalvar(ModelUsuario usuario, ModelSenhasSalvas senha, ModelSenhasSalvas senhaTela) {
+		em.getTransaction().begin();
+		
 		UsuarioDAO usuarioDAO = new UsuarioDAO(em);
 		SenhasSalvasDAO senhasSalvasDAO = new SenhasSalvasDAO(em);
 		
 		if(usuario.senhaJaExiste(senha)) {
 			senha.setDescricao(senhaTela.getDescricao());
 			senhasSalvasDAO.update(senha);
-			usuarioDAO.update(usuario);
 		}else {
+			criptografarSenha(senhaTela);
 			senhaTela.setUsuario(usuario);
 			usuario.salvarSenha(senhaTela);
 			senhasSalvasDAO.insert(senhaTela);
-			usuarioDAO.update(usuario);
 		}
 
+		usuarioDAO.insertOrUpdate(usuario);
 		salvarSenhaView.exibeMensagemInformativa("Senha salva com sucesso!");
 		salvarSenhaView.dispose();
+		
+		em.getTransaction().commit();
+	}
+
+	private void criptografarSenha(ModelSenhasSalvas senhaTela) {
+		senhaTela.setSenha(Criptografar.criptografar(senhaTela.getSenha()));
 	}
 
 	public void sair(SalvarSenhaView salvarSenhaView) {
